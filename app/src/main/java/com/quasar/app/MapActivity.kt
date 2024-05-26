dd00d02b35e83a0baf368989de3e7301415d003c
Matt Chapman <matthew.chapman@xero.com>
2024-05-26 16:30:45 +1200

    Change to drawer layout

--- app/src/main/java/com/quasar/app/MapActivity.kt
+++ app/src/main/java/com/quasar/app/MapActivity.kt
@@ -5,19 +5,37 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExitToApp
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.firebase.ui.auth.AuthUI
 import com.quasar.app.ui.theme.QUASARTheme
+import kotlinx.coroutines.launch
 
 class MapActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,9 +43,7 @@ class MapActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             QUASARTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MapScreen(logout = { logout() })
-                }
+                MapScreen(logout = { logout() })
             }
         }
     }
@@ -41,30 +57,64 @@ class MapActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MapScreen(logout: () -> Unit, modifier: Modifier = Modifier) {
-    Column {
-        Text("MAP SCREEN")
-        Button(onClick = {
-            logout()
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet {
+                Text("QUASAR", modifier = Modifier.padding(16.dp))
+                Divider()
+                NavigationDrawerItem(
+                    icon = { Icon(Icons.Filled.ExitToApp, "") },
+                    label = { Text(text = stringResource(id = R.string.log_out)) },
+                    selected = false,
+                    onClick = { logout() })
+            }
         }) {
-            Text(text = stringResource(R.string.log_out))
+        Scaffold(topBar = {
+            TopAppBar(title = { Text(text = "QUASAR") }, navigationIcon = {
+                IconButton(onClick = {
+                    scope.launch {
+                        drawerState.apply {
+                            if (isClosed) open() else close()
+                        }
+                    }
+                }) {
+                    Icon(Icons.Filled.Menu, contentDescription = "Menu")
+                }
+            })
+        }) { contentPadding ->
+
         }
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Text(text = "MAP ACTIVITY")
+        }
+
     }
+//    Column {
+//        Text("MAP SCREEN")
+//        Button(onClick = {
+//            logout()
+//        }) {
+//            Text(text = stringResource(R.string.log_out))
+//        }
+//    }
 }
 
-@Composable
-fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Map activity!!! $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview3() {
     QUASARTheme {
-        Greeting2("Android")
+//        Greeting2("Android")
     }
 }
