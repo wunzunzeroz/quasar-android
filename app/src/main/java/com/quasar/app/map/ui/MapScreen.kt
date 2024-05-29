6be347ba7eb2d09c7a65a2f448fa98c5460c6c31
Matt Chapman <matthew.chapman@xero.com>
2024-05-29 18:24:31 +1200

    Make top bar transparent

--- app/src/main/java/com/quasar/app/map/ui/MapScreen.kt
+++ app/src/main/java/com/quasar/app/map/ui/MapScreen.kt
@@ -13,17 +13,21 @@ import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -68,17 +72,25 @@ fun MapScreen(navController: NavHostController) {
         }) {
             Scaffold(
                 topBar = {
-                    TopAppBar(title = { Text(text = "QUASAR") }, navigationIcon = {
-                        IconButton(onClick = {
-                            scope.launch {
-                                drawerState.apply {
-                                    if (isClosed) open() else close()
+                    TopAppBar(
+                        title = { Text(text = "QUASAR") },
+                        navigationIcon = {
+                            IconButton(onClick = {
+                                scope.launch {
+                                    drawerState.apply {
+                                        if (isClosed) open() else close()
+                                    }
                                 }
+                            }) {
+                                Icon(Icons.Filled.Menu, contentDescription = "Menu", tint = MaterialTheme.colorScheme.onPrimary)
                             }
-                        }) {
-                            Icon(Icons.Filled.Menu, contentDescription = "Menu")
-                        }
-                    })
+                        },
+                        colors = TopAppBarDefaults.topAppBarColors(
+                            containerColor = Color.Transparent,
+                            titleContentColor = MaterialTheme.colorScheme.onPrimary,
+                            actionIconContentColor = MaterialTheme.colorScheme.onPrimary
+                        )
+                    )
                 },
 //                bottomBar = {
 //                    BottomAppBar() {
@@ -103,7 +115,8 @@ fun MapScreen(navController: NavHostController) {
                     },
                     onMapLongClickListener = {
                         Log.d(
-                            logTag, "User long tapped map at Lat/Lng: ${it.latitude()}/${it.longitude()}"
+                            logTag,
+                            "User long tapped map at Lat/Lng: ${it.latitude()}/${it.longitude()}"
                         )
 
                         true
