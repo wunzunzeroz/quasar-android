34f32723d6e5d37c4081110854c68c0c700b7add
Matt Chapman <matthew.chapman@xero.com>
2024-06-16 12:12:33 +1200

    Update styling

--- app/src/main/java/com/quasar/app/map/components/SelectMapStyleSheet.kt
+++ app/src/main/java/com/quasar/app/map/components/SelectMapStyleSheet.kt
@@ -6,7 +6,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DirectionsBoat
 import androidx.compose.material.icons.filled.Flight
@@ -19,6 +22,8 @@ import androidx.compose.material.icons.filled.Waves
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -26,16 +31,18 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.quasar.app.R
 import com.quasar.app.map.styles.MapStyle
 
 @Composable
 fun SelectMapStyleSheet(onMapStyleSelected: (MapStyle) -> Unit, modifier: Modifier = Modifier) {
-    Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = modifier) {
-        Text(stringResource(R.string.select_map_style_sheet_title))
+    Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = modifier.padding(horizontal = 16.dp)) {
+        Text(stringResource(R.string.select_map_style_sheet_title), style = MaterialTheme.typography.headlineSmall)
+        Spacer(modifier = Modifier.height(8.dp))
         Divider()
-        Spacer(modifier = Modifier.height(4.dp))
+        Spacer(modifier = Modifier.height(12.dp))
         StyleButton(icon = Icons.Filled.Hiking,
             label = "Outdoors",
             onClick = { onMapStyleSelected(MapStyle.Outdoors) })
@@ -56,10 +63,16 @@ fun SelectMapStyleSheet(onMapStyleSelected: (MapStyle) -> Unit, modifier: Modifi
 
 @Composable
 private fun StyleButton(icon: ImageVector, label: String, onClick: () -> Unit) {
-    TextButton(onClick = onClick) {
-        Row(horizontalArrangement = Arrangement.SpaceBetween) {
+    OutlinedButton(onClick = onClick, modifier = Modifier.fillMaxWidth()) {
+        Row(horizontalArrangement = Arrangement.Center, verticalAlignment = Alignment.CenterVertically, modifier = Modifier.fillMaxWidth() ) {
             Icon(icon, "")
+            Spacer(modifier = Modifier.width(8.dp))
             Text(label)
         }
     }
+}
+@Preview(showSystemUi = true)
+@Composable
+fun SelectMapStyleSheetPreview() {
+    SelectMapStyleSheet(onMapStyleSelected = {})
 }
