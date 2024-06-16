995bcfe51031ec27b56ac4ec1a9bccc9d9706dab
Matt Chapman <matthew.chapman@xero.com>
2024-06-17 08:52:24 +1200

    Update login screen

--- app/src/main/java/com/quasar/app/auth/ui/LoginScreen.kt
+++ app/src/main/java/com/quasar/app/auth/ui/LoginScreen.kt
@@ -2,17 +2,27 @@ package com.quasar.app.auth.ui
 
 import android.app.Activity
 import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
@@ -43,21 +53,48 @@ fun LandingScreen(
     val intent =
         AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build()
 
-    Box(modifier = modifier) {
+    Box(modifier = modifier.background(MaterialTheme.colorScheme.primaryContainer)) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center,
             modifier = Modifier.fillMaxSize()
         ) {
-            Text(stringResource(R.string.welcome_message))
-            Button(onClick = {
-                scope.launch {
-                    signInLauncher.launch(intent)
+            Text(
+                "QUASAR",
+                style = MaterialTheme.typography.displayMedium,
+                color = MaterialTheme.colorScheme.onPrimaryContainer
+            )
+            Text(
+                "Navigate. Locate. Communicate.",
+                color = MaterialTheme.colorScheme.onPrimaryContainer
+            )
+
+            Spacer(modifier = Modifier.height(64.dp))
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 64.dp)
+            ) {
+                Button(
+                    onClick = {
+                        scope.launch {
+                            signInLauncher.launch(intent)
+                        }
+                    }, modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(text = stringResource(R.string.login_button))
                 }
+                OutlinedButton(
+                    onClick = {
+                        scope.launch {
+                            signInLauncher.launch(intent)
+                        }
+                    }, modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text("Sign up")
 
-            }) {
-                Text(text = stringResource(R.string.login_button))
-
+                }
             }
         }
     }
@@ -80,4 +117,10 @@ private fun onSignInResult(
         // Handle sign-in failure
         throw Exception("Error logging in")
     }
+}
+
+@Composable
+@Preview(showSystemUi = true)
+fun LoginScreenPreview() {
+    LandingScreen(navController = NavHostController(LocalContext.current))
 }
