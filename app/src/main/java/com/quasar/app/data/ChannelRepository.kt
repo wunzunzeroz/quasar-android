57c84fc053c3781248fec9cc9e9acabebba4a02a
Matt Chapman <matthew.chapman@xero.com>
2024-07-07 17:58:00 +1200

    005 Fix up channel repository

--- app/src/main/java/com/quasar/app/data/ChannelRepository.kt
+++ app/src/main/java/com/quasar/app/data/ChannelRepository.kt
@@ -1,6 +1,5 @@
 package com.quasar.app.data
 
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FieldPath
 import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.ktx.firestore
@@ -9,9 +8,9 @@ import com.google.firebase.firestore.toObject
 import com.google.firebase.firestore.toObjects
 import com.google.firebase.ktx.Firebase
 import com.quasar.app.domain.models.Channel
-import com.quasar.app.channels.models.FirebaseChannelMember
+import com.quasar.app.domain.models.ChannelMember
+import com.quasar.app.domain.models.User
 import com.quasar.app.channels.models.CreateChannelInput
-import com.quasar.app.channels.models.UserDetails
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flatMapLatest
@@ -22,106 +21,52 @@ import kotlinx.coroutines.tasks.await
 import java.util.UUID
 
 interface ChannelRepository {
-    val channels: Flow<List<Channel>>
-
-    suspend fun createChannel(channelInput: CreateChannelInput): String
-    suspend fun joinChannel(channelId: String)
+    fun getChannelsForUser(userChannels: Flow<List<String>>): Flow<List<Channel?>>
     fun getChannel(channelId: String): Flow<Channel?>
-    fun getUserChannels(userId: String): Flow<List<String>>
+    suspend fun createChannel(input: CreateChannelInput, user: User): String
+    suspend fun addMemberToChannel(channelId: String, user: User)
 }
 
 class ChannelRepositoryImpl() : ChannelRepository {
-    private val channelCollection = "channels"
-    private val userCollection = "users"
-
     private val db = Firebase.firestore
-
-    override val channels: Flow<List<Channel>>
-        get() = getChannels(getUserDetails().userId)
-
-    override fun getUserChannels(userId: String): Flow<List<String>> =
-        db.collection("users").document(userId).snapshots()
-            .mapNotNull { it.toObject<User>()?.channels ?: emptyList() }
+    private val collectionName = "channels"
 
     @OptIn(ExperimentalCoroutinesApi::class)
-    fun getChannels(userId: String): Flow<List<Channel>> =
-        getUserChannels(userId).flatMapLatest { joinedChannels ->
-            if (joinedChannels.isEmpty()) {
+    override fun getChannelsForUser(userChannels: Flow<List<String>>): Flow<List<Channel?>> {
+        return userChannels.flatMapLatest { channelIds ->
+            if (channelIds.isEmpty()) {
                 flowOf(emptyList())
             } else {
-                db.collection("channels").whereIn(FieldPath.documentId(), joinedChannels)
+                db.collection(collectionName).whereIn(FieldPath.documentId(), channelIds)
                     .snapshots().mapNotNull { it.toObjects<Channel>() }
             }
         }
+    }
 
     override fun getChannel(channelId: String): Flow<Channel?> {
-        return db.collection("channels").document(channelId).snapshots()
+        return db.collection(collectionName).document(channelId).snapshots()
             .map { it.toObject<Channel>() }
     }
 
-    override suspend fun createChannel(channelInput: CreateChannelInput): String {
-        val userDetails = getUserDetails()
-        addUserIfNotExists()
+    override suspend fun createChannel(input: CreateChannelInput, user: User): String {
+        val channelMember = ChannelMember(userId = user.id, name = user.name)
 
-        val firebaseChannelMember = FirebaseChannelMember(userDetails.userId, userDetails.name)
+        val channelId = UUID.randomUUID().toString() // TODO - Create friendly UUIDs
 
-        val channelId = UUID.randomUUID().toString()
-        val channel =
-            Channel(channelId, channelInput.name, channelInput.description, memberCount = 1)
-        db.collection(channelCollection).document(channelId).set(channel).await()
+        val channel = Channel(
+            channelId, input.name, input.description, members = listOf(channelMember)
+        )
 
-        // Add user ID to channel's 'members' array
-        db.collection(channelCollection).document(channelId).collection("members")
-            .document(userDetails.userId).set(firebaseChannelMember).await()
-
-        // Add channel ID to user object's 'channels' array
-        db.collection(userCollection).document(userDetails.userId)
-            .update("channels", FieldValue.arrayUnion(channelId)).await()
+        db.collection(collectionName).document(channelId).set(channel).await()
 
         return channelId
     }
 
-    override suspend fun joinChannel(channelId: String) {
-        val userDetails = getUserDetails()
+    override suspend fun addMemberToChannel(channelId: String, user: User) {
+        val channelMember = ChannelMember(userId = user.id, name = user.name)
 
-        val userDoc = db.collection("users").document(userDetails.userId)
-
-        userDoc.update("channels", FieldValue.arrayUnion(channelId)).await()
-//        val firebaseChannelMember = FirebaseChannelMember(userDetails.userId, userDetails.name)
-//
-//
 //        // TODO - Handle channel doesn't exist
-//        val channelRef = db.collection(channelCollection).document(channelId)
-//
-//        channelRef.collection("members").document(userDetails.userId).set(firebaseChannelMember)
-//            .await()
-//        channelRef.update("memberCount", FieldValue.increment(1)).await()
-    }
-
-    private suspend fun addUserIfNotExists() {
-        val userDetails = getUserDetails()
-        val userDocRef = db.collection(userCollection).document(userDetails.userId)
-
-        db.runTransaction { transaction ->
-            val snapshot = transaction.get(userDocRef)
-
-            if (!snapshot.exists()) {
-                transaction.set(userDocRef, userDetails)
-            }
-        }.await()
-    }
-
-    private fun getUserDetails(): UserDetails {
-        val user = FirebaseAuth.getInstance().currentUser
-            ?: throw IllegalStateException("User is not authenticated")
-
-        return UserDetails(user.uid, user.email.toString(), user.displayName.toString())
+        db.collection(collectionName).document(channelId)
+            .update("members", FieldValue.arrayUnion(channelMember)).await()
     }
 }
-
-data class User(
-    val userId: String = "",
-    val email: String = "",
-    val name: String = "",
-    val channels: List<String> = emptyList()
-)
